08213b5e06410cd07ccbfddd422946c069c6ca2b
yasmiinie <ly_zaidi@esi.dz>
2024-02-16 12:45:27 +0100

    done

--- index.py
+++ index.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import qrcode
+import os
 
-# Function to generate QR code for a given ID
-def generate_qr_code(data):
+# Function to generate QR code for a given ID and name
+def generate_qr_code(row):
+    data = f"{row['Name']}"
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -12,14 +14,19 @@ def generate_qr_code(data):
     qr.add_data(data)
     qr.make(fit=True)
     img = qr.make_image(fill_color="black", back_color="white")
-    img.save(f"qrcodes/{data}.png")  # Save the QR code with the ID as the filename
-    return f"qrcodes/{data}.png"
+    
+    # Ensure the "qrcodes" folder exists
+    os.makedirs("qrcodes", exist_ok=True)
+
+    file_name = f"qrcodes/{data}.png"
+    img.save(file_name)
+    return file_name
 
 # Read CSV file
 df = pd.read_csv("QR.csv")
 
 # Add a new column with the QR code links
-df['QRCodeLink'] = df['ID'].apply(generate_qr_code)
+df['QRCodeLink'] = df.apply(generate_qr_code, axis=1)
 
 # Save the updated DataFrame to a new CSV file
 df.to_csv("output_file.csv", index=False)
